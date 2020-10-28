d7091587ea5a029fc745ebdd5505386844effb24
HearSilent <hear.silent1995@gmail.com>
2020-10-28 17:32:27 +0800

    Migrate to AndroidX

--- app/src/main/java/hearsilent/amazingavatar/libs/AppBarStateChangeListener.java
+++ app/src/main/java/hearsilent/amazingavatar/libs/AppBarStateChangeListener.java
@@ -1,6 +1,6 @@
 package hearsilent.amazingavatar.libs;
 
-import android.support.design.widget.AppBarLayout;
+import com.google.android.material.appbar.AppBarLayout;
 
 public abstract class AppBarStateChangeListener implements AppBarLayout.OnOffsetChangedListener {
 
